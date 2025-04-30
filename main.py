566cf2ff76b46cfbde7bdbfd58b76e1d5a20bd7b
blackkk805 <ghosttnito@gmail.com>
2025-05-01 01:43:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -506,4 +506,4 @@ async def send_requests(tokens, hex_encrypted_data, request_type):
     
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
